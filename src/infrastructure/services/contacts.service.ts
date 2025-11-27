106c7007688250be7108b03ee6509f600f82ff7d
Erick <eeramirez@tuvansa.com.mx>
2025-11-27 13:10:44 -0600

    add new contacts

--- src/infrastructure/services/contacts.service.ts
+++ src/infrastructure/services/contacts.service.ts
@@ -27,21 +27,26 @@ export class ContactService {
       name: 'Erick',
       email: "eeramirez@tuvansa.com.mx"
     },
-    // {
-    //   phone: '5216243828879',
-    //   name: 'Luis Quintero',
-    //   email: "lquintero@tuvansa.com.mx"
-    // },
-    // {
-    //   phone: '525541141306',
-    //   name: 'Roy Grinberg',
-    //   email: "rgrinberg@tuvansa.com.mx"
-    // },
-    // {
-    //   phone: '525522406714',
-    //   name: 'Marcos Avalos',
-    //   email: "mavalos@tuvansa.com.mx"
-    // },
+    {
+      phone: '5216243828879',
+      name: 'Luis Quintero',
+      email: "lquintero@tuvansa.com.mx"
+    },
+    {
+      phone: '525541141306',
+      name: 'Roy Grinberg',
+      email: "rgrinberg@tuvansa.com.mx"
+    },
+    {
+      phone: '525522406714',
+      name: 'Marcos Avalos',
+      email: "mavalos@tuvansa.com.mx"
+    },
+    {
+      phone: "528116603993",
+      name: "Alejandro Lozano",
+      email: "alozano@tuvansa.com.mx"
+    }
   ]
 
   constructor(
